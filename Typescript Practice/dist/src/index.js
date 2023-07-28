3d9403324e201b4f9a621766efcdf13c6bd8add2
Ky Nguyen <kynguyen@Kys-MacBook-Pro.local>
2023-07-28 13:17:38 -0700

    tested typescript debugging

---
+++ Typescript Practice/dist/src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let age = 20;
+if (age < 50)
+    age += 10;
+console.log(age);
+//# sourceMappingURL=index.js.map
---
+++ Typescript Practice/src/index.ts
@@ -0,0 +1,6 @@
+let age: number = 20;
+
+if (age < 50) 
+    age+=10;
+console.log(age)
+
