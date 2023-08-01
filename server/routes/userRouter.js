787add4c57974dd2dda3ceec02fd820815453c1e
kenniford <kim.yb.kenny@gmail.com>
2023-08-01 16:51:09 -0500

    refactored backend file structure

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,13 +1,17 @@
-var express = require('express');
-var userRouter = express.Router();
+const express = require('express');
+const userRouter = express.Router();
+const userController = require('../controllers/userController.js')
+
 //routers go here
-userRouter.post('/signup', userController.createUser, function (req, res) {
-    return res.status(200).json(res.locals.newUser);
+userRouter.post('/signup', userController.createUser, (req, res) => {
+  return res.status(200).json(res.locals.newUser);
 });
-userRouter.post('/login', userController.login, function (req, res) {
-    return res.status(200).json({
-        message: 'Login successful!',
-        username: res.locals.username
-    });
+
+userRouter.post('/login', userController.login, (req, res) => {
+  return res.status(200).json({
+    message: 'Login successful!',
+    username: res.locals.username,
+  });
 });
+
 module.exports = userRouter;
