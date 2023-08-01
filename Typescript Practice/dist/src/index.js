0639bae5e8548fee8c96f1359a00420299a451f8
Ky Nguyen <kynguyen@Kys-MacBook-Pro.local>
2023-08-01 13:09:02 -0700

    removed Komodo folder

--- Typescript Practice/dist/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function getCustomer(id) {
-    return id === 0 ? null : { birthday: new Date() };
-}
-let customer = getCustomer(1);
-console.log(customer === null || customer === void 0 ? void 0 : customer.birthday);
-//# sourceMappingURL=index.js.map
