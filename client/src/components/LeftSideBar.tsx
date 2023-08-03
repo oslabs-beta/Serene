df860454a1630e72d7d03018c6366e46a8cc0454
dahliarianna <dahliarianna@gmail.com>
2023-08-03 11:22:41 -0500

    testing metric display

--- client/src/components/LeftSideBar.tsx
+++ client/src/components/LeftSideBar.tsx
@@ -17,58 +17,63 @@ const LeftSideBar = (props: Props) => {
 
   return (
     <div>
+      {showSidebar ? (
+        <button
+          className="flex text-4xl text-black items-center cursor-pointer fixed left-8 top-6 z-50"
+          onClick={() => setShowSidebar(!showSidebar)}
+        >
+          x
+        </button>
+      ) : (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" strokeWidth={1.5} stroke="currentColor"   
+        onClick={() => setShowSidebar(!showSidebar)}
+        className="fixed  z-40 flex items-center cursor-pointer left-4 top-2"
+        viewBox="0 -5 90 80"
+        width="150"
+        height="150"
+      >
+        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+        </svg>
+      )}
 
+      <div //entire sidebar div // sidebar does not fully extend to the bottom, research 
+        className={`flex flex-col items-center -left-2 -top-1 -mb-4 w-[25vw] min-w-min bg-gray-300  pl-10 pr-10 text-black fixed h-full z-40  ease-in-out duration-300 border-2 border-black ${
+        showSidebar ? "translate-x-0 " : "-translate-x-full"
+        }`}
+      >
+      <h3 className="mt-20 text-2xl min-w-min font-semibold text-white mb-5">F U N C T I O N S</h3>
+        <div // function data table in sidebar
+        className="border-8 border-black overflow-y-auto h-[75%] w-[110%]"
+        > 
+          {/* <div> */}
+     { mockFuncDetails.map( (item) => (
+              <FunctionDetails 
+              detailID={item.id}
+              name={item.name}
+              description={item.description}
+              versHist={item.versHist}
+              metric={item.metric}
+              warmData={item.warmData}
+              />
+          )
+          )}
+                    { mockFuncDetails.map( (item) => (
+              <FunctionDetails 
+              detailID={item.id}
+              name={item.name}
+              description={item.description}
+              versHist={item.versHist}
+              metric={item.metric}
+              warmData={item.warmData}
+              />
+          )
+          )}
 
-    
-  {showSidebar ? (
-    <button
-      className="flex text-4xl text-black items-center cursor-pointer fixed left-8 top-6 z-50"
-      onClick={() => setShowSidebar(!showSidebar)}
-    >
-      x
-    </button>
-  ) : (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" strokeWidth={1.5} stroke="currentColor"   
-    onClick={() => setShowSidebar(!showSidebar)}
-    className="fixed  z-40 flex items-center cursor-pointer left-4 top-2"
-    viewBox="0 0 100 80"
-    width="200"
-    height="200"
-  >
-    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
-  </svg>
-  )}
-
-
-  {/* <div className="top-0 left-0 w-[35vw] bg-green-500  p-10 pl-20 text-white fixed h-full z-30">
-    <h3 className="mt-20 text-4xl font-semibold text-white">Functions</h3>
-  </div> */}
-
-  <div //entire sidebar div
-  className={`top-0 left-0 w-[25vw] bg-blue-600  p-10 pl-20 text-white fixed h-full z-40  ease-in-out duration-300 border-8 border-yellow-500 ${
-    showSidebar ? "translate-x-0 " : "-translate-x-full"
-  }`}
->
-  <h3 className="mt-20 text-4xl font-semibold text-white">I am a sidebar</h3>
-  <div> 
-    FUNCTIONS GO HERE
-    { mockFuncDetails.map( (item) => (
-            <FunctionDetails 
-            detailID={item.id}
-            name={item.name}
-            description={item.description}
-            versHist={item.versHist}
-            metric={item.metric}
-            warmData={item.warmData}
-            />
-        )
-   )}
-  </div>
-
+          {/* </div> */}
+     
+        </div>
+      </div>
     </div>
-  </div>
-
-
   )
 }
 
