90350e1ed286a4d647ce88005ffa0e0a3400932e
dahliarianna <dahliarianna@gmail.com>
2023-08-10 12:35:55 -0500

    version history added

--- client/src/components/VersionHistory.tsx
+++ client/src/components/VersionHistory.tsx
@@ -1,7 +1,13 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState, useEffect} from 'react'
 import LeftSideBar from './LeftSideBar'
 import RightSideBar from './RightSidebar'
 import { useLocation } from "react-router-dom"
+import {
+  Popover,
+  PopoverHandler,
+  PopoverContent,
+  Button,
+} from "@material-tailwind/react";
 
 type Props = {
   // name: string;
@@ -10,15 +16,70 @@ type Props = {
 }
 
 const VersionHistory = () => {
+  const [isLoading, setIsLoading] = useState(true);
 
+  // const [popoverViewing, setPopoverViewing] = useState(Array(mockVersionData))
+
+  useEffect(() => {
+    setTimeout( () => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
+
+  const mockCode1 = () => { 
+      const arr = ['info1','info2','info3','info4', 'info5']
+      Math.floor(arr.length/2)
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify('error fixed')
+      };
+      return response;
+  }
+  const mockCode2 = () => { 
+    const arr = ['info1','info2','info3','info4', 'info5']
+    Math.floor(arr.length/2)
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify('error fixed')
+    };
+    return response;
+}
+const mockCode3 = () => { 
+  const arr = ['info1','info2','info3','info4', 'info5']
+  Math.floor(arr.length/2)
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('error fixed')
+  };
+  return response;
+}
+const mockCode4 = () => { 
+  const arr = ['info1','info2','info3','info4', 'info5']
+  Math.floor(arr.length/2)
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('error fixed')
+  };
+  return response;
+}
+const mockCode5 = () => { 
+  const arr = ['info1','info2','info3','info4', 'info5']
+  Math.floor(arr.length/2)
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('error fixed')
+  };
+  return response;
+}
     const mockVersionData = [
-      { version:1 ,alias: 'test1', arn: 'xxx:hellouniverse:test'},
-      { version:2, alias: 'test2', arn: 'xxx:hellouniverse:test'},
-      { version:3, alias: 'test3', arn: 'xxx:hellouniverse:test'},
-      { version:4, alias: 'test4', arn: 'xxx:hellouniverse:test'},
-      { version:5, alias: 'test5', arn: 'xxx:hellouniverse:test'},
+      { version:1 ,alias: 'test1', arn: 'xxx:hellouniverse:test', code: mockCode1},
+      { version:2, alias: 'test2', arn: 'xxx:hellouniverse:test', code: mockCode2},
+      { version:3, alias: 'test3', arn: 'xxx:hellouniverse:test', code: mockCode3},
+      { version:4, alias: 'test4', arn: 'xxx:hellouniverse:test', code: mockCode4},
+      { version:5, alias: 'test5', arn: 'xxx:hellouniverse:test', code: mockCode5},
     ]
-
+    console.log('is this code? ', mockVersionData[0].code)
     // const location = useLocation()
     // const data = location.state.name
     // console.log('this is data in versions ', data)
@@ -37,7 +98,23 @@ const VersionHistory = () => {
         <RightSideBar />  
         </div> 
         <div className="flex justify-center">
+        
+        {/* {isLoading ? (
+         <div
+  className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+  role="status">
+  <span
+    className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+    >Loading...</span >
+</div> 
 
+        ) : ( 
+          
+        )}
+         */}
+        
+        
+        
         <a
             href="/home"
             className="w-64 rounded-md px-3.5 py-2 m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-black text-black text-white text-center"
@@ -82,23 +159,48 @@ const VersionHistory = () => {
         
  {/*BODY DIV */}
  
-<div className="flex justify-center border-4 border-green-300 h-screen w-full ">
+<div className="flex justify-center h-screen w-full ">
    
-  <div className="flex flex-col justify-between border-4 border-red-400 h-1/2 w-1/2">
+  <div className="flex flex-col justify-between my-3 border-4 bg-gray-200 rounded-md h-2/3 w-3/4 ">
     {mockVersionData.reverse().map((item) => (
-      <div className="flex justify-between"> {/* Remove justify-between class */}
-        <div></div> {/*HERE for SPACING ONLY*/}
-        <button className='border-2 border-black '> 
-          version {item.version}
-        </button>
-        <span className=""> {/* Add ml-auto class to align the span to the right */}
+      <div className="group flex justify-start"> {/* Remove justify-between class */}
+        <div></div> HERE for SPACING ONLY
+        <Popover placement="left">
+          <PopoverHandler className='w-20 mr-auto p-1 mt-2 mb-1 transition duration-100 ease-in-out group-hover:scale-150'>
+            <Button>version {item.version}</Button>
+          </PopoverHandler>
+          <PopoverContent className='border-black border-2'>
+            <span>more version {item.code.toString()} details here</span>
+          </PopoverContent>
+        </Popover>
+
+
+<div className="relative w-10 h-20 transition-all duration-200 ease-in-out transform-growth group-hover:w-80 ml-auto">
+      <svg className="w-full h-full overflow-visible">
+        <defs>
+          <marker id="m" markerWidth="4" markerHeight="8" refX="0" refY="1" viewBox="0 0 1 2">
+            <polygon points="0,0 1,1 0,2" fill="black"/>
+          </marker>
+        </defs>
+        <line x1="0" y1="50%" x2="100%" y2="50%" strokeWidth="2" markerEnd="url(#m)" stroke="black"/>
+      </svg>
+    </div>
+
+    
+        <span className="group-hover:bg-black group-hover:text-white bg-white rounded-md p-2 transition duration-100 ease-in-out group-hover:scale-110 text-center flex items-center ml-auto"> {/* Add ml-auto class to align the span to the right */}
           alias {item.alias}
         </span>
       </div>
     ))}
+
+
+
+
+
+
+
+
   </div>
-
-
 
 
 
